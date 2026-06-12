839e7a68c427c4b97ba22841f940da96db9f4204
rishwana08 <rishwanas410@gmail.com>
2026-06-12 11:24:53 +0530

    Added Operators

---
+++ Operators.js
@@ -0,0 +1,32 @@
+var a = 20
+var b = 15
+console.log("Assignment Operator");
+
+//Addition
+console.log(a+b);
+//Subtraction
+console.log(a-b);
+//Multiplication
+console.log(a*b);
+//Division
+console.log(a/b);
+//Modulus 
+console.log(a%b);
+//Exponentiation
+console.log(a**b);
+
+console.log("Assignment operator");
+
+console.log(a+=10);
+console.log(a-=10);
+console.log(a*=10);
+console.log(a/=10);
+console.log(a%=10);
+console.log(a**=10);
+
+
+
+
+
+
+

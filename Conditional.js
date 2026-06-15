630f3f3ca13d8353952516c592f9e2be8892c94b
rishwana08 <rishwanas410@gmail.com>
2026-06-15 10:40:46 +0530

    Added Conditional Statements

--- Conditional.js
+++ Conditional.js
@@ -1,14 +1,34 @@
+console.log("if else Statement");
+
 var light = "Green";
 
 if (light == "Green") {
     console.log("Go!");
 } 
 else if (light == "Yellow") {
-    console.log("Slow down!");
+    console.log("Wait!");
 } 
 else if (light == "Red") {
     console.log("Stop!");
 } 
 else {
     console.log("Invalid Color!!");
+}
+
+console.log("Switch Statement");
+let signal = "White"
+
+switch (signal) {
+    case "Green":
+        console.log("Go!");
+        break;
+    case "Yellow":
+        console.log("Wait!");
+        break;
+    case "Red":
+        console.log("Stop!");
+        break;
+    default:
+        console.log("Invalid Color!");
+        break;
 }
